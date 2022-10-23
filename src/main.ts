475549984c19327667fb3bb7e3716e7b79b71f6f
matyas97 <matyaskrecek@icloud.com>
2022-10-23 22:05:51 +0200

    Changed default port and url to 127.0.0.1:8000

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,9 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+  const port = 8080;
   await app.listen(port);
 
-  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+  Logger.log(`🚀 Application is running on: http://127.0.0.1:${port}`);
 }
 bootstrap();
