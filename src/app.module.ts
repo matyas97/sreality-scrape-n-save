cd330b16098d365d7a36f63c19b5d3f4d565d2a5
matyas97 <matyaskrecek@icloud.com>
2022-10-23 16:22:22 +0200

    Replaced Prisma with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import AppConfig from './app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScraperModule } from './scraper/scraper.module';
+import { PropertyModule } from './property/property.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Property } from './property/property.entity';
 
 @Module({
   imports: [
@@ -11,6 +14,17 @@ import { ScraperModule } from './scraper/scraper.module';
       load: [AppConfig],
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'dev',
+      entities: [Property],
+      synchronize: true,
+    }),
+    PropertyModule,
     ScraperModule,
   ],
   controllers: [AppController],
