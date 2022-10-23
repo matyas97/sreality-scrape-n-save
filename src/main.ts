c414b6c414a2238d8b9bcd18ca57ce5e445a8ee7
matyas97 <matyaskrecek@icloud.com>
2022-10-23 19:18:03 +0200

    Template engine setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
   await app.listen(port);
