03d9f25760762e6d27e01837a478293071c9fd31
matyas97 <matyaskrecek@icloud.com>
2022-10-23 19:20:11 +0200

    Root url renders index.hbs with Property data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { PropertyService } from './property/property.service';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly propertyService: PropertyService) {}
+
+  async getProperties() {
+    const properties = await this.propertyService.listProperties();
+
+    const formatedProperties = properties.map((property) => {
+      return {
+        ...property,
+        url: `https://www.sreality.cz${property.url}`,
+      };
+    });
+
+    return { properties: formatedProperties };
   }
 }
